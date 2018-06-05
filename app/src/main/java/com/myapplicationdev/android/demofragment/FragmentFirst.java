d01eb97966ac4a1a54ef31be4c045ab8d36310fc
Wen-New <16022742@myrp.edu.sg>
2018-06-05 15:05:55 +0800

    Updated

--- app/src/main/java/com/myapplicationdev/android/demofragment/FragmentFirst.java
+++ app/src/main/java/com/myapplicationdev/android/demofragment/FragmentFirst.java
@@ -1,13 +1,12 @@
 package com.myapplicationdev.android.demofragment;
 
-
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,33 +14,31 @@ import android.view.ViewGroup;
 public class FragmentFirst extends Fragment {
 
     android.widget.Button btnAddText;
-  android.widget.TextView tvFrag1;
+    android.widget.TextView tvFrag1;
 
     public FragmentFirst() {
         // Required empty public constructor
     }
 
-
-
-  @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-    View view = inflater.inflate(R.layout.fragment_first, container, false);
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
 
-    tvFrag1 = (android.widget.TextView) view.findViewById(R.id.tvFrag1);
-    btnAddText = (android.widget.Button) view.findViewById(R.id.btnAddTextFrag1);
+        tvFrag1 = (TextView) view.findViewById(R.id.tvFrag1);
+        btnAddText = (Button) view.findViewById(R.id.btnAddTextFrag1);
 
-    btnAddText.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        String data = tvFrag1.getText().toString() + "\n" + "New Data";
-        tvFrag1.setText(data);
-      }
-    });
+        btnAddText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = tvFrag1.getText().toString() + "\n" + "New Data";
+                tvFrag1.setText(data);
+            }
+        });
 
-    return view;
-  }
+        return view;
+    }
 
 
 }
--- app/src/main/java/com/myapplicationdev/android/demofragment/FragmentSecond.java
+++ app/src/main/java/com/myapplicationdev/android/demofragment/FragmentSecond.java
@@ -1,13 +1,12 @@
 package com.myapplicationdev.android.demofragment;
 
-
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,33 +14,31 @@ import android.view.ViewGroup;
 public class FragmentSecond extends Fragment {
 
     android.widget.Button btnAddText;
-  android.widget.TextView tvFrag2;
+    android.widget.TextView tvFrag2;
 
     public FragmentSecond() {
         // Required empty public constructor
     }
 
-
-
-  @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-    View view = inflater.inflate(R.layout.fragment_second, container, false);
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
 
-    tvFrag2 = (android.widget.TextView) view.findViewById(R.id.tvFrag2);
-    btnAddText = (android.widget.Button) view.findViewById(R.id.btnAddTextFrag2);
+        tvFrag2 = (TextView) view.findViewById(R.id.tvFrag2);
+        btnAddText = (Button) view.findViewById(R.id.btnAddTextFrag2);
 
-    btnAddText.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        String data = tvFrag2.getText().toString() + "\n" + "New Data F2";
-        tvFrag2.setText(data);
-      }
-    });
+        btnAddText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = tvFrag2.getText().toString() + "\n" + "New Data F2";
+                tvFrag2.setText(data);
+            }
+        });
 
-    return view;
-  }
+        return view;
+    }
 
 
 }
